56ca90e99c36fdb343d6ce13fcc17137c1e642e3
hzfangwentian <hzfangwentian@corp.netease.com>
2018-09-30 17:07:22 +0800

    update

---
+++ generators/app/index.js
@@ -0,0 +1,27 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    initializing() {
+        this.log('Welcome to use this scaffolding tool')
+    }
+
+    prompting() {
+
+    }
+
+    configuring() {
+
+    }
+
+    writing() {
+
+    }
+
+    install() {
+
+    }
+
+    end() {
+        this.log('Generate files done')
+    }
+};
